baa71b618c972b1db81f69edffc436ba5e755e40
ccc53021 <chengcheng.chang@mail.sdu.edu.cn>
2025-05-28 16:43:23 +0800

    Add files via upload

---
+++ gift-64/r_19/em/key_dependencies/get_key_conditions_by_trails.py
@@ -0,0 +1,327 @@
+
+import numpy
+# from sage.all import *
+import round_key_transition
+import generate_tables as tables
+
+rks = round_key_transition.key_recover_bit_from_top_to_bottom_64
+
+GIFT_SBOX = [0x1, 0xa, 0x4, 0xc, 0x6, 0xf, 0x3, 0x9, 0x2, 0xd, 0xb, 0x7, 0x5, 0x0, 0x8, 0xe]
+n = 4
+m = 4
+sbox = GIFT_SBOX
+state_bits = 64
+state_words = 16
+sbox_bits = 4
+key_bits = 128
+
+round_constants = [0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3E, 0x3D, 0x3B, 0x37, 0x2F, 0x1E, 0x3C, 0x39, 0x33, 0x27, 0x0E, 0x1D, 0x3A, 0x35, 0x2B, 0x16, 0x2C, 0x18, 0x30, 0x21, 0x02, 0x05, 0x0B, 0x17, 0x2E, 0x1C, 0x38]
+round_constant_positions = [3, 7, 11, 15, 19, 23]
+
+def get_DDT(sbox, n, m):
+    DDT = numpy.zeros((2 ** n, 2 ** m))
+
+    for input_d in range(2 ** n):
+        for output_d in range(2 ** m):
+            for x in range(2 ** n):
+                y = x ^ input_d
+                sx = sbox[x]
+                sy = sbox[y]
+                if (sx ^ sy) == output_d:
+                    DDT[input_d, output_d] += 1
+
+    return DDT
+
+
+def get_1d_by_hex(x):
+    y = [0] * state_bits
+    for i in range(state_bits):
+        y[i] = (x >> i) & 0x1
+
+    return y
+
+def vector_inner_product(u, x, n):
+    left = 0
+    for i in range(n):
+        left += ((u >> i) & 0x1) * ((x >> i) & 0x1)
+    left = left % 2
+
+    return left
+
+def get_master_key_by_round_key(round, round_position, key_recovery_round_top):
+    true_round = round + 1 + key_recovery_round_top
+
+    if state_bits == 128:
+        if round_position % 2 == 0:
+            true_position = round_position // 2
+            master_key = round_key_transition.key_recover_bit_from_top_to_bottom_128[true_round][true_position]
+            return master_key
+        elif round_position % 2 == 1:
+            true_position = (round_position - 1) // 2
+            master_key = round_key_transition.key_recover_bit_from_top_to_bottom_128[true_round][true_position]
+            return master_key
+    elif state_bits == 64:
+        if round_position % 2 == 0:
+            true_position = round_position // 2
+            master_key = round_key_transition.key_recover_bit_from_top_to_bottom_64[true_round][true_position]
+            return master_key
+        elif round_position % 2 == 1:
+            true_position = ((round_position - 1) // 2) + 1
+            master_key = round_key_transition.key_recover_bit_from_top_to_bottom_64[true_round][true_position]
+            return master_key
+
+def key_conditions_by_one_trail(iii, diff_and_trail, begin_round, round_upper, round_lower):
+    one_trail_key_bit = key_bits
+    one_key_expression = [0] * one_trail_key_bit * 3
+    correlation = 1
+    for round in range(round_upper + round_lower):
+        # sb
+
+        u0 = diff_and_trail[3 * round][0]
+        u1 = diff_and_trail[3 * round][1]
+        u2 = diff_and_trail[3 * round][2]
+        v0 = diff_and_trail[3 * round + 1][0]
+        v1 = diff_and_trail[3 * round + 1][1]
+        v2 = diff_and_trail[3 * round + 1][2]
+
+        for i in range(0, state_words):
+            uu0 = (u0 >> (sbox_bits * i)) & (2 ** sbox_bits - 1)
+            uu1 = (u1 >> (sbox_bits * i)) & (2 ** sbox_bits - 1)
+            uu2 = (u2 >> (sbox_bits * i)) & (2 ** sbox_bits - 1)
+            vv0 = (v0 >> (sbox_bits * i)) & (2 ** sbox_bits - 1)
+            vv1 = (v1 >> (sbox_bits * i)) & (2 ** sbox_bits - 1)
+            vv2 = (v2 >> (sbox_bits * i)) & (2 ** sbox_bits - 1)
+
+            if round < round_upper:
+                c = tables.get_ddt_upper_item_by_given_all(sbox, n, m, uu0, vv0, uu1, vv1, uu2, vv2)
+                correlation *= c
+            elif round == round_upper:
+                c = tables.get_bct_item_by_given_all(sbox, n, m, uu0, vv0, uu1, vv1, uu2, vv2)
+                correlation *= c
+            else:
+                c = tables.get_ddt_lower_item_by_given_all(sbox, n, m, uu0, vv0, uu1, vv1, uu2, vv2)
+                correlation *= c
+
+        rcs = round_constants[round + begin_round]
+        mask_c = (1 << (state_bits - 1)) | (((rcs >> 5) & 0x1) << round_constant_positions[5]) | (
+                ((rcs >> 4) & 0x1) << round_constant_positions[4]) | (
+                         ((rcs >> 3) & 0x1) << round_constant_positions[3]) | (
+                         ((rcs >> 2) & 0x1) << round_constant_positions[2]) | (
+                         ((rcs >> 1) & 0x1) << round_constant_positions[1]) | (
+                         ((rcs >> 0) & 0x1) << round_constant_positions[0])
+        correlation *= ((-1) ** bin(mask_c & v0).count('1'))
+
+        k0 = diff_and_trail[3 * round + 2][0]
+        k1 = diff_and_trail[3 * round + 2][1]
+        k2 = diff_and_trail[3 * round + 2][2]
+        for i in range(state_words):
+            aa_x = (k0 >> i * sbox_bits) & (2 ** sbox_bits - 1)
+            for j in range(sbox_bits):
+                if state_bits == 64:
+                    if j != 2 and j != 3:
+                        bb_x = (aa_x >> j) & 0x1
+                        if bb_x == 1:
+                            round_key_full = round * state_bits + (sbox_bits * i + j)
+                            round_key_round = round_key_full // state_bits
+                            round_key_position = round_key_full % state_bits
+                            master_key = get_master_key_by_round_key(round_key_round, round_key_position,
+                                                                     begin_round)
+                            master_key_word = master_key[0]
+                            master_key_position = master_key[1]
+                            master_key_full = master_key_word * 16 + master_key_position
+                            one_key_expression[master_key_full] ^= 1
+
+            if round < round_upper:
+                aa_upper = (k2 >> i * sbox_bits) & (2 ** sbox_bits - 1)
+                for j in range(sbox_bits):
+                    if state_bits == 64:
+                        if j != 2 and j != 3:
+                            bb_upper = (aa_upper >> j) & 0x1
+                            if bb_upper == 1:
+                                round_key_full = round * state_bits + (sbox_bits * i + j)
+                                round_key_round = round_key_full // state_bits
+                                round_key_position = round_key_full % state_bits
+                                master_key = get_master_key_by_round_key(round_key_round, round_key_position,
+                                                                         begin_round)
+                                master_key_word = master_key[0]
+                                master_key_position = master_key[1]
+                                master_key_full = master_key_word * 16 + master_key_position
+                                one_key_expression[master_key_full + one_trail_key_bit] ^= 1
+
+            else:
+                aa_lower = (k1 >> i * sbox_bits) & (2 ** sbox_bits - 1)
+                for j in range(sbox_bits):
+                    if state_bits == 64:
+                        if j != 2 and j != 3:
+                            bb_lower = (aa_lower >> j) & 0x1
+                            if bb_lower == 1:
+                                round_key_full = round * state_bits + (sbox_bits * i + j)
+                                round_key_round = round_key_full // state_bits
+                                round_key_position = round_key_full % state_bits
+                                master_key = get_master_key_by_round_key(round_key_round, round_key_position,
+                                                                         begin_round)
+                                master_key_word = master_key[0]
+                                master_key_position = master_key[1]
+                                master_key_full = master_key_word * 16 + master_key_position
+                                one_key_expression[master_key_full + 2 * one_trail_key_bit] ^= 1
+
+    key_str = ""
+    for i in range(one_trail_key_bit):
+        if one_key_expression[i] == 1:
+            word = i // 16
+            position = i % 16
+            key_str += "k{}_{} +".format(word, position)
+    for i in range(one_trail_key_bit, 2 * one_trail_key_bit):
+        if one_key_expression[i] == 1:
+            word = (i - one_trail_key_bit) // 16
+            position = (i - one_trail_key_bit) % 16
+            key_str += "delta_upper_k_{}_{} + ".format(word, position)
+    for i in range(2 * one_trail_key_bit, 3 * one_trail_key_bit):
+        if one_key_expression[i] == 1:
+            word = (i - one_trail_key_bit * 2) // 16
+            position = (i - one_trail_key_bit * 2) % 16
+            key_str += "delta_lower_k_{}_{} + ".format(word, position)
+
+    one_key_expression1 = one_key_expression[::]
+    if correlation < 0:
+        one_key_expression1.append(1)
+        key_str += " = 1"
+    else:
+        one_key_expression1.append(0)
+        key_str += " = 0"
+
+    return one_key_expression, one_key_expression1, key_str
+
+def count_valid_by_given_basis(key_list1, basis, basis_values):
+    key_0 = [0] * (key_bits * 3 + 1)
+    key_1 = [0] * (key_bits * 3 + 1)
+    key_1[key_bits * 3] = 1
+    count_mask_0_sign_0 = 0
+    count_mask_0_sign_1 = 0
+    count_mask_1_sign_0 = 0
+    count_mask_1_sign_1 = 0
+    trails_mask_0_sign_0 = []
+    trails_mask_0_sign_1 = []
+    trails_mask_1_sign_0 = []
+    trails_mask_1_sign_1 = []
+    for i in range(len(key_list1)):
+        one_key = key_list1[i]
+        if one_key == key_0:
+            count_mask_0_sign_0 += 1
+            trails_mask_0_sign_0.append(i + 1)
+        elif one_key == key_1:
+            count_mask_0_sign_1 += 1
+            trails_mask_0_sign_1.append(i + 1)
+        else:
+            sign = 0
+            one_key_list = []
+            for j in range(key_bits * 3):
+                if one_key[j] == 1:
+                    one_key_list.append([j // 16, j % 16])
+            for j in range(len(basis)):
+                one_basis = basis[j]
+                one_value = basis_values[j]
+                flag_y = 1
+
+                # for ob in one_basis:
+                #     if ob not in one_key_list:
+                #         flag_y = 0
+                #         break
+                pp_k_list = one_basis
+                one_basis_k_x = pp_k_list[0]
+                one_basis_k_upper = pp_k_list[1]
+                one_basis_k_lower = pp_k_list[2]
+                for one_key_pp in one_basis_k_x:
+                    if one_key_pp not in one_key_list:
+                        flag_y = 0
+                        break
+                for one_key_pp in one_basis_k_upper:
+                    new_position = one_key_pp[0] * 16 + one_key_pp[1] + key_bits
+                    if [new_position // 16, new_position % 16] not in one_key_list:
+                        flag_y = 0
+                        break
+                for one_key_pp in one_basis_k_lower:
+                    new_position = one_key_pp[0] * 16 + one_key_pp[1] + key_bits * 2
+                    if [new_position // 16, new_position % 16] not in one_key_list:
+                        flag_y = 0
+                        break
+
+                if flag_y == 1:
+                    sign ^= one_value
+            sign ^= one_key[key_bits]
+            if sign == 0:
+                count_mask_1_sign_0 += 1
+                trails_mask_1_sign_0.append(i)
+            else:
+                count_mask_1_sign_1 += 1
+                trails_mask_1_sign_1.append(i)
+
+    print("no key , +1 : {}".format(count_mask_0_sign_0))
+    print(trails_mask_0_sign_0)
+    print("no key , -1 : {}".format(count_mask_0_sign_1))
+    print(trails_mask_0_sign_1)
+    print("xor key, +1 : {}".format(count_mask_1_sign_0))
+    print(trails_mask_1_sign_0)
+    print("xor key, -1 : {}".format(count_mask_1_sign_1))
+    print(trails_mask_1_sign_1)
+    all = count_mask_0_sign_0 + count_mask_1_sign_0 - count_mask_0_sign_1 - count_mask_1_sign_1
+    print("all is {}".format(all))
+
+    return all
+
+import quasi_bcs_all as diffs_and_quasis
+quasi_bcs = diffs_and_quasis.routes
+print(len(quasi_bcs))
+print(len(quasi_bcs[0]))
+
+route_numbers = len(quasi_bcs)
+max_w = len(quasi_bcs[0])
+
+begin_round = 9
+round_upper = 1
+round_lower = 1
+key_list = []
+key_list1 = []
+key_list1_with_w = [[] for i in range(max_w)]
+count_w_and_numbers = [0] * max_w
+for rn in range(route_numbers):
+    one_route_quasi_bcs = quasi_bcs[rn]
+    for ww in range(max_w):
+        dts_w = one_route_quasi_bcs[ww]
+        if len(dts_w) > 0:
+            print()
+            print("---------------------------------------------------------------")
+            print("route {}, w {} : {} quasi-bcs".format(rn, ww, len(dts_w)))
+            count_w_and_numbers[ww] += len(dts_w)
+            for i in range(len(dts_w)):
+                one_quasi_bc = dts_w[i]
+                one_key_expression, one_key_expression1, key_str = key_conditions_by_one_trail(i + 1, one_quasi_bc, begin_round, round_upper, round_lower)
+                print("l {} : {}".format(i + 1, key_str))
+                key_list.append(one_key_expression)
+                key_list1.append(one_key_expression1)
+                key_list1_with_w[ww].append(one_key_expression1)
+print()
+print("all {} trails".format(len(key_list)))
+
+import gaussian_elimination
+rank, rank1, key_rref_str, key_space = gaussian_elimination.get_rank_and_base_master_key(key_list, key_list1, key_bits)
+
+count_non_zero = []
+condition_bits = 0
+count_w_and_numbers_in_fixed_key = [0] * max_w
+for i in range(2 ** 0):
+    basis = [    ]
+    basis_values = []
+    print()
+    print("the {}-th basis : {}".format(i, basis))
+    count_p_all = 0
+    for ww in range(max_w):
+        if len(key_list1_with_w[ww]) > 0:
+            count_w_and_numbers_in_fixed_key[ww] = count_valid_by_given_basis(key_list1_with_w[ww], basis, basis_values)
+            print("w = {} : {} quasi-bcs, {} quasi-bcs in fixed-key, p_w = 2^-{} * {}".format(ww, count_w_and_numbers[ww], count_w_and_numbers_in_fixed_key[ww], ww, count_w_and_numbers_in_fixed_key[ww]))
+            count_p_all += count_w_and_numbers_in_fixed_key[ww] * (2 ** (-ww))
+    print()
+    print("p = {} = 2^-{}".format(count_p_all, -numpy.log2(count_p_all)))
+print()
+print("--------------------------------")
+
---
+++ gift-64/r_19/lower/key_dependencies/quasi_bcs_all.py
@@ -0,0 +1,363 @@
+routes = [
+# route 0
+[
+# Solution: of weight 0
+[
+     # No trails with weight equal to 0.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 1
+[
+     # No trails with weight equal to 1.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 2
+[
+     # No trails with weight equal to 2.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 3
+[
+     # No trails with weight equal to 3.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 4
+[
+     # No trails with weight equal to 4.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 5
+[
+     # No trails with weight equal to 5.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 6
+[
+     # No trails with weight equal to 6.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 7
+[
+     # No trails with weight equal to 7.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 8
+[
+     # No trails with weight equal to 8.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 9
+[
+     # No trails with weight equal to 9.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 10
+[
+     # No trails with weight equal to 10.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 11
+[
+     # No trails with weight equal to 11.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 12
+[
+     # No trails with weight equal to 12.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 13
+[
+     # No trails with weight equal to 13.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 14
+[
+     # No trails with weight equal to 14.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 15
+[
+     # No trails with weight equal to 15.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 16
+[
+     # No trails with weight equal to 16.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 17
+[
+     # No trails with weight equal to 17.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 18
+[
+     # No trails with weight equal to 18.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 19
+[
+     # No trails with weight equal to 19.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 20
+[
+     # No trails with weight equal to 20.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 21
+[
+     # No trails with weight equal to 21.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 22
+[
+    # all: 22
+    # Solution: [#1 of weight 22]
+    [
+     # upper: 22
+     [0x0000000000000000, 0x0000000000000000, 0x0a00000000000000],
+     # after sb
+     [0x0000000000000000, 0x0000000000000000, 0x0100000000000000],
+     # after p
+     [0x0000000000000000, 0x0000000000000000, 0x0000100000000000],
+     # after k
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after sb
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after p
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after k
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after sb
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after p
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after k
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000010000],
+     # after sb
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000080000],
+     # after p
+     [0x0000000000000000, 0x0000000000000000, 0x0080000000000000],
+     # after k
+     [0x0000000000000000, 0x0000000000000000, 0x0090000000000000],
+     # after sb
+     [0x0000000000000000, 0x0000000000000000, 0x0010000000000000],
+     # after p
+     [0x0000000000000000, 0x0000000000000000, 0x1000000000000000],
+     # after k
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after sb
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after p
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after k
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after sb
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after p
+     [0x0000000000000000, 0x0000000000000000, 0x0000000000000000],
+     # after k
+     [0x0000000000000000, 0x0000000000000000, 0x0000000100000000],
+     # after sb
+     [0x0000000000000000, 0x0000000000000000, 0x0000000800000000],
+     # after p
+     [0x0000000000000000, 0x0000000000000000, 0x0800000000000000],
+     # after k
+
+     [0x0000000000000000, 0x0000000000000000, 0x0800000000000010],
+     # sign = -1: 0
+     # sign = +1: 1
+    ],
+     # No trails with weight equal to 22.
+     # sign = -1: 0
+     # sign = +1: 1
+],
+# Solution: of weight 23
+[
+     # No trails with weight equal to 23.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 24
+[
+     # No trails with weight equal to 24.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 25
+[
+     # No trails with weight equal to 25.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 26
+[
+     # No trails with weight equal to 26.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 27
+[
+     # No trails with weight equal to 27.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 28
+[
+     # No trails with weight equal to 28.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 29
+[
+     # No trails with weight equal to 29.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 30
+[
+     # No trails with weight equal to 30.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 31
+[
+     # No trails with weight equal to 31.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 32
+[
+     # No trails with weight equal to 32.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 33
+[
+     # No trails with weight equal to 33.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 34
+[
+     # No trails with weight equal to 34.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 35
+[
+     # No trails with weight equal to 35.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 36
+[
+     # No trails with weight equal to 36.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 37
+[
+     # No trails with weight equal to 37.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 38
+[
+     # No trails with weight equal to 38.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 39
+[
+     # No trails with weight equal to 39.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 40
+[
+     # No trails with weight equal to 40.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 41
+[
+     # No trails with weight equal to 41.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 42
+[
+     # No trails with weight equal to 42.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 43
+[
+     # No trails with weight equal to 43.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 44
+[
+     # No trails with weight equal to 44.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 45
+[
+     # No trails with weight equal to 45.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 46
+[
+     # No trails with weight equal to 46.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 47
+[
+     # No trails with weight equal to 47.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 48
+[
+     # No trails with weight equal to 48.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+# Solution: of weight 49
+[
+     # No trails with weight equal to 49.
+     # sign = -1: 0
+     # sign = +1: 0
+],
+],
+
+]
