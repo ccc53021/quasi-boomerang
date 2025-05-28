caf46ab2794989930d7ceb28950075a206df7c7d
ccc53021 <chengcheng.chang@mail.sdu.edu.cn>
2025-05-28 15:54:49 +0800

    Add files via upload

---
+++ skinny-64/skinny-64-128/r_17/upper/u0_v0_zeros/routes_clustering.py
@@ -0,0 +1,439 @@
+routes = [
+# Solution: of weight 0
+[
+     # No trails with weight equal to 0.
+     # all is 0
+],
+# Solution: of weight 1
+[
+     # No trails with weight equal to 1.
+     # all is 0
+],
+# Solution: of weight 2
+[
+     # No trails with weight equal to 2.
+     # all is 0
+],
+# Solution: of weight 3
+[
+     # No trails with weight equal to 3.
+     # all is 0
+],
+# Solution: of weight 4
+[
+     # No trails with weight equal to 4.
+     # all is 0
+],
+# Solution: of weight 5
+[
+     # No trails with weight equal to 5.
+     # all is 0
+],
+# Solution: of weight 6
+[
+    # all: 6
+    # Solution: [#1 of weight 6]
+    [
+     0x1000000000000000,
+     # after sb
+     0xa000000000000000,
+     # after k
+     0xa000000000000000,
+     # after sr
+     0x0a00000000000000,
+     # after mc
+     0x0000000000000a00,
+     # after sb
+     0x0000000000000500,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x000000000b000000,
+     # after sr
+     0x00000000b0000000,
+     # after mc
+     0x0000b00000000000,
+     # after sb
+     0x0000100000000000,
+     # after k
+     0x0000100000000000,
+     # after sr
+     0x0000001000000000,
+     # after mc
+
+     0x0010001000000010,
+    ],
+    # all: 6
+    # Solution: [#2 of weight 6]
+    [
+     0x1000000000000000,
+     # after sb
+     0x8000000000000000,
+     # after k
+     0x8000000000000000,
+     # after sr
+     0x0800000000000000,
+     # after mc
+     0x0000000000000800,
+     # after sb
+     0x0000000000000500,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x000000000b000000,
+     # after sr
+     0x00000000b0000000,
+     # after mc
+     0x0000b00000000000,
+     # after sb
+     0x0000100000000000,
+     # after k
+     0x0000100000000000,
+     # after sr
+     0x0000001000000000,
+     # after mc
+
+     0x0010001000000010,
+    ],
+    # all: 6
+    # Solution: [#3 of weight 6]
+    [
+     0x1000000000000000,
+     # after sb
+     0x9000000000000000,
+     # after k
+     0x9000000000000000,
+     # after sr
+     0x0900000000000000,
+     # after mc
+     0x0000000000000900,
+     # after sb
+     0x0000000000000500,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x0000000000000000,
+     # after sr
+     0x0000000000000000,
+     # after mc
+     0x0000000000000000,
+     # after sb
+     0x0000000000000000,
+     # after k
+     0x000000000b000000,
+     # after sr
+     0x00000000b0000000,
+     # after mc
+     0x0000b00000000000,
+     # after sb
+     0x0000100000000000,
+     # after k
+     0x0000100000000000,
+     # after sr
+     0x0000001000000000,
+     # after mc
+
+     0x0010001000000010,
+    ],
+     # No trails with weight equal to 6.
+     # all is 3
+],
+# Solution: of weight 7
+[
+     # No trails with weight equal to 7.
+     # all is 0
+],
+# Solution: of weight 8
+[
+     # No trails with weight equal to 8.
+     # all is 0
+],
+# Solution: of weight 9
+[
+     # No trails with weight equal to 9.
+     # all is 0
+],
+# Solution: of weight 10
+[
+     # No trails with weight equal to 10.
+     # all is 0
+],
+# Solution: of weight 11
+[
+     # No trails with weight equal to 11.
+     # all is 0
+],
+# Solution: of weight 12
+[
+     # No trails with weight equal to 12.
+     # all is 0
+],
+# Solution: of weight 13
+[
+     # No trails with weight equal to 13.
+     # all is 0
+],
+# Solution: of weight 14
+[
+     # No trails with weight equal to 14.
+     # all is 0
+],
+# Solution: of weight 15
+[
+     # No trails with weight equal to 15.
+     # all is 0
+],
+# Solution: of weight 16
+[
+     # No trails with weight equal to 16.
+     # all is 0
+],
+# Solution: of weight 17
+[
+     # No trails with weight equal to 17.
+     # all is 0
+],
+# Solution: of weight 18
+[
+     # No trails with weight equal to 18.
+     # all is 0
+],
+# Solution: of weight 19
+[
+     # No trails with weight equal to 19.
+     # all is 0
+],
+# Solution: of weight 20
+[
+     # No trails with weight equal to 20.
+     # all is 0
+],
+# Solution: of weight 21
+[
+     # No trails with weight equal to 21.
+     # all is 0
+],
+# Solution: of weight 22
+[
+     # No trails with weight equal to 22.
+     # all is 0
+],
+# Solution: of weight 23
+[
+     # No trails with weight equal to 23.
+     # all is 0
+],
+# Solution: of weight 24
+[
+     # No trails with weight equal to 24.
+     # all is 0
+],
+# Solution: of weight 25
+[
+     # No trails with weight equal to 25.
+     # all is 0
+],
+# Solution: of weight 26
+[
+     # No trails with weight equal to 26.
+     # all is 0
+],
+# Solution: of weight 27
+[
+     # No trails with weight equal to 27.
+     # all is 0
+],
+# Solution: of weight 28
+[
+     # No trails with weight equal to 28.
+     # all is 0
+],
+# Solution: of weight 29
+[
+     # No trails with weight equal to 29.
+     # all is 0
+],
+# Solution: of weight 30
+[
+     # No trails with weight equal to 30.
+     # all is 0
+],
+# Solution: of weight 31
+[
+     # No trails with weight equal to 31.
+     # all is 0
+],
+# Solution: of weight 32
+[
+     # No trails with weight equal to 32.
+     # all is 0
+],
+# Solution: of weight 33
+[
+     # No trails with weight equal to 33.
+     # all is 0
+],
+# Solution: of weight 34
+[
+     # No trails with weight equal to 34.
+     # all is 0
+],
+# Solution: of weight 35
+[
+     # No trails with weight equal to 35.
+     # all is 0
+],
+# Solution: of weight 36
+[
+     # No trails with weight equal to 36.
+     # all is 0
+],
+# Solution: of weight 37
+[
+     # No trails with weight equal to 37.
+     # all is 0
+],
+# Solution: of weight 38
+[
+     # No trails with weight equal to 38.
+     # all is 0
+],
+# Solution: of weight 39
+[
+     # No trails with weight equal to 39.
+     # all is 0
+],
+# Solution: of weight 40
+[
+     # No trails with weight equal to 40.
+     # all is 0
+],
+# Solution: of weight 41
+[
+     # No trails with weight equal to 41.
+     # all is 0
+],
+# Solution: of weight 42
+[
+     # No trails with weight equal to 42.
+     # all is 0
+],
+# Solution: of weight 43
+[
+     # No trails with weight equal to 43.
+     # all is 0
+],
+# Solution: of weight 44
+[
+     # No trails with weight equal to 44.
+     # all is 0
+],
+# Solution: of weight 45
+[
+     # No trails with weight equal to 45.
+     # all is 0
+],
+# Solution: of weight 46
+[
+     # No trails with weight equal to 46.
+     # all is 0
+],
+# Solution: of weight 47
+[
+     # No trails with weight equal to 47.
+     # all is 0
+],
+# Solution: of weight 48
+[
+     # No trails with weight equal to 48.
+     # all is 0
+],
+# Solution: of weight 49
+[
+     # No trails with weight equal to 49.
+     # all is 0
+],
+],
+
